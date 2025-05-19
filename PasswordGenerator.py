0b7ac540051ae53999c35f202a897803e8e0a997
Arush Singh <113715134+arushsingh0604@users.noreply.github.com>
2025-05-19 14:39:26 +0530

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,4 +1,4 @@
-
+#Easy version
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -21,5 +21,34 @@ for s in range(0,nr_numbers):
 
 print("your password is:",password)
 
+#Hard version
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+pass_list = []
+for l in range(0,nr_letters):
+    pass_list.append(random.choice(letters))
+
+for s in range(0,nr_symbols):
+    pass_list.append(random.choice(symbols))
+
+for n in range(0,nr_numbers):
+    pass_list.append(random.choice(numbers))
+
+print(pass_list)
+random.shuffle(pass_list)
+print(pass_list)
+
+password = ""
+for char in pass_list:
+    password += char
+print(password)
 
 
